e04f31e07023a4deb3db9b9830a43c3c89edd490
Athanasios Kefalas <a.kefalas@msensis.com>
2024-03-23 12:28:05 +0200

    Package cleanup.

--- Package.swift
+++ Package.swift
@@ -12,11 +12,7 @@ let package = Package(
         .library(
             name: "StitcherMacros",
             targets: ["StitcherMacros"]
-        ),
-        .executable(
-            name: "StitcherMacrosClient",
-            targets: ["StitcherMacrosClient"]
-        ),
+        )
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-syntax.git", from: "509.0.0"),
@@ -26,7 +22,7 @@ let package = Package(
         // Targets can depend on other targets in this package and products from dependencies.
         // Macro implementation that performs the source transformation of a macro.
         .macro(
-            name: "StitcherMacrosMacros",
+            name: "StitcherMacrosPlugins",
             dependencies: [
                 .product(name: "SwiftSyntaxMacros", package: "swift-syntax"),
                 .product(name: "SwiftCompilerPlugin", package: "swift-syntax")
@@ -34,16 +30,13 @@ let package = Package(
         ),
 
         // Library that exposes a macro as part of its API, which is used in client programs.
-        .target(name: "StitcherMacros", dependencies: ["StitcherMacrosMacros"]),
-
-        // A client of the library, which is able to use the macro in its own code.
-        .executableTarget(name: "StitcherMacrosClient", dependencies: ["StitcherMacros"]),
+        .target(name: "StitcherMacros", dependencies: ["StitcherMacrosPlugins"]),
 
         // A test target used to develop the macro implementation.
         .testTarget(
             name: "StitcherMacrosTests",
             dependencies: [
-                "StitcherMacrosMacros",
+                "StitcherMacrosPlugins",
                 .product(name: "SwiftSyntaxMacrosTestSupport", package: "swift-syntax"),
             ]
         ),
