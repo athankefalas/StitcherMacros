6689ae827df0c1ac11623ae0af298f3a3ef4eae7
Athanasios Kefalas <sakiskefalas@yahoo.gr>
2024-04-08 13:48:15 +0300

    Release version 1.1.0

--- Package.swift
+++ Package.swift
@@ -23,8 +23,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-syntax.git", from: "509.0.0"),
-//        .package(url: "https://github.com/athankefalas/Stitcher.git", exact: "1.0.0"),
-        .package(url: "https://github.com/athankefalas/Stitcher.git", branch: "feature/remove-combine")
+        .package(url: "https://github.com/athankefalas/Stitcher.git", exact: "1.1.0")
     ],
     targets: [
         
